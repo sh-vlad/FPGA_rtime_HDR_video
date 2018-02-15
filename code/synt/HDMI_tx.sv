97df14d1775691a85376ba1d5e5a1f3fa71b3971
S_H_V_L_A_D <shvladspb@gmail.com>
2018-02-15 21:33:02 +0300

    t2 resync_fifo added

--- code/synt/HDMI_tx.sv
+++ code/synt/HDMI_tx.sv
@@ -13,7 +13,7 @@ module HDMI_tx
 	input wire							reset_n,
 // sink interface
 	input wire							asi_snk_valid_i,
-	output wire							asi_snk_ready_o,
+	output reg							asi_snk_ready_o,
 	input wire		[DATA_WIDTH-1: 0] 	asi_snk_data_i,
 	input wire		[ 0: 0] 			asi_snk_empty_i,
 	input wire							asi_snk_startofpacket_i,
@@ -21,12 +21,12 @@ module HDMI_tx
 	input wire							asi_snk_error_i,
 	input wire		[ 0: 0] 			asi_snk_channel_i,	
 //vga interface to ADV7513
-	output reg		[ 7: 0]				data_enable,
-	output reg		[ 7: 0]				hsync,
-	output reg		[ 7: 0]				vsync,
-	output reg		[ 7: 0]				data_r,
-	output reg		[ 7: 0]				data_g,
-	output reg		[ 7: 0]				data_b
+	output reg							data_enable,
+	output reg							hsync,
+	output reg							vsync,
+	output wire		[ 7: 0]				data_r,
+	output wire		[ 7: 0]				data_g,
+	output wire		[ 7: 0]				data_b
 	
 );
 //
@@ -46,7 +46,11 @@ localparam VTOTAL	= VBLANK + VACTIVE;
 
 reg		[12: 0]		h_count;
 reg		[12: 0]		v_count;	
+reg		[12: 0]		string_num;
+wire	[10: 0]  	wrusedw;
+reg					rdreq;
 
+//making strobes 
 always @( posedge pixel_clk or negedge reset_n )
 	if ( !reset_n )
 		h_count <= 13'h0;
@@ -66,13 +70,13 @@ always @( posedge pixel_clk or negedge reset_n )
 			v_count	<= v_count + 1'h1;
 			
 always @( posedge pixel_clk )
-	if ( ( h_count >= HFRONT  ) && ( h_count >= HFRONT + HSYNC ) ) 
+	if ( ( h_count >= HFRONT  ) && ( h_count <= HFRONT + HSYNC ) ) 
 		hsync  <= 1'h0;
 	else
 		hsync  <= 1'h1;
 			
 always @( posedge pixel_clk )	
-	if ( ( h_count <= HBLANK ) || ( v_count <= VBLANK ) )
+	if ( ( h_count >= 13'h0 && h_count <= HBLANK ) || ( v_count <= VBLANK ) )
 		data_enable	<= 1'h0;
 	else
 		data_enable	<= 1'h1;
@@ -82,11 +86,46 @@ always @( posedge pixel_clk )
 		vsync <= 1'h0;
 	else
 		vsync <= 1'h1;
+		
+always @( posedge pixel_clk )
+	if ( ( h_count >= 13'h0 && h_count <= HBLANK - 1) || ( v_count <= VBLANK ) )
+		rdreq	<= 1'h0;
+	else
+		rdreq	<= 1'h1;		
+//
+
+always @( posedge clk or negedge reset_n )
+	if ( !reset_n )	
+		string_num	<= 	13'h0;
+	else
+		if ( asi_snk_startofpacket_i )
+			string_num	<= string_num  + 1'h1;
+		else if ( asi_snk_startofpacket_i == 13'd720 )
+			string_num	<= 	13'h1;
+
+always @( posedge clk )
+	if ( wrusedw <= HACTIVE + 1 )
+		asi_snk_ready_o <= 1'h1;
+	else
+		asi_snk_ready_o <= 1'h0;
 	
+resync_fifo_HDMI_tx resync_fifo_HDMI_tx_inst_0
+(
+	.data		( asi_snk_data_i			),
+	.rdclk      ( pixel_clk					),
+	.rdreq      ( rdreq						),
+	.wrclk      ( clk						),
+	.wrreq      ( asi_snk_valid_i			),
+	.q          ({ data_b, data_g, data_r }	),
+	.rdempty    (							),
+	.rdfull     (							),
+	.rdusedw	(							),
+	.wrempty    (							),
+	.wrfull     (							),
+	.wrusedw	( wrusedw					)
+);			
 
 endmodule
-
-
 
 
 
